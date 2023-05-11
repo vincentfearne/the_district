5e967e2ee9bc25f059ecef2e693f06bc4298b877
Fearne <vincent.fearne@gmail.com>
2023-05-11 14:17:04 +0200

    11.05

---
+++ src/Controller/CrudController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Plat;
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\form;
+
+class CrudController extends AbstractController
+{
+    #[Route('/categorie/nouveau', 'categorie.new', methods: ['GET', 'POST'])]
+    public function newcategorie() : Response
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+        return $this->render('crud/newcategorie.html.twig', [
+        'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/plat/nouveau', 'plat.new', methods: ['GET', 'POST'])]
+    public function newplat(Request $request) : Response
+    {
+        $plat = new Plat();
+        $form = $this->createForm(PlatType::class, $plat);
+
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+            {
+
+            }
+            else {
+
+                }
+        
+        return $this->render('crud/newplat.html.twig', [
+        'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/CategorieType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class, [
+                'attr' => [
+                    'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '250'
+                ],
+                'label' => 'Libelle',
+                'label_attr' => ['class' => 'form-label mt-4'],
+            ])
+            ->add('image')
+            ->add('active')
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'],
+                    'label' => 'Ajouter la catégorie'
+            ])
+            ->add('image', FileType::class, [
+                'label' => false,
+                'mapped' => false,
+                'required' => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
